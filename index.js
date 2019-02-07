d06ee83382e13ce818ba7cb7ce974022206f1eed
Carlos Barrera <barrera9696@gmail.com>
2019-02-07 05:10:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ function yourAnimal() {
    return animal
    console.log(yourAnimal());
 }
-const add2 = 2
+
 function add2(n) {
+  const add2 = 2
   return n + two
-} console.log(add2 + n + two);
+} 
 
   // Feel free to move things around!
 
