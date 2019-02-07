d318e48d1e5f2780587a613da9a548aadc93eb84
Carlos Barrera <barrera9696@gmail.com>
2019-02-07 04:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
    return animal
-   console.log(yourAnimal() === 'cat');
+   console.log(yourAnimal());
 }
 
 
